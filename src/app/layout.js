e6cf457d2328749a99255ff0387324ff19cc045f
Gouenji10 <excelblade10@gmail.com>
2024-02-13 18:08:00 +0545

    error fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,10 +6,6 @@ import '@/styles/style.scss';
 import Header from '@/components/header/Header';
 import Footer from '@/components/footer/Footer';
 import { Next13NProgress } from 'nextjs13-progress';
-import { fetchProductCategories } from '@/query/products';
-import StoreProvider from '@/store/providers/StoreProvider';
-import ThemeProvider from '@/store/providers/ThemeProvider';
-import { getSessionToken } from '@/query/session';
 
 export const metadata = {
 	title: 'CGS Mart',
@@ -22,13 +18,11 @@ export default async function RootLayout({ children }) {
 			<body className='page-body'>
 				<MantineProvider>
 					<Notifications position="top-right" zIndex={1000} />
-					<StoreProvider>
-						<ThemeProvider>
+					
 							<div className='page-wrapper'>{children}</div>
 							<Footer />
 							<Next13NProgress color="#C96" height={3} />
-						</ThemeProvider>
-					</StoreProvider>
+					
 				</MantineProvider>
 			</body>
 		</html >
